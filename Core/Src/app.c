4dde41a93f19e0955f8daa874dd71a189888e145
downy <112371402+downy25@users.noreply.github.com>
2024-12-03 11:35:14 +0900

    Update app.c

--- Core/Src/app.c
+++ Core/Src/app.c
@@ -59,35 +59,3 @@ void app(){
 		}
 	}
 }
-
-//내가 작성한 코드
-
-/*void app(){
-	uint16_t delay=0;
-	static uint16_t count=0;
-	//uart 장치 초기화
-	initUart(&huart2);
-	//pwm시작
-	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
-	//timer 시작
-	HAL_TIM_IC_Start(&htim3, TIM_CHANNEL_2);
-	HAL_TIM_Base_Start(&htim11); //base는 기본기능만 사용
-	while(1){
-		setSound(500); //500hz음 발생
-		printf("distance = %d\n",getDistance());
-		if(getDistance()>=20)delay=1000;
-		else if(getDistance()>=15&&getDistance()<20)delay=800;
-		else if(getDistance()>=10&&getDistance()<15)delay=600;
-		else if(getDistance()>=5&&getDistance()<10)delay=400;
-		else delay=0;
-		HAL_Delay(delay);
-		stopSound();
-		HAL_Delay(delay);
-
-	}
-}*/
-
-//HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET); //GPIO_PIN_RESET == 0
-//HAL_Delay(1000);
-//HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);  //GPIO_PIN_SET == 1
-//HAL_Delay(1000);
